ccffd621d75eb06cd052d431d0e4e340a1569013
jinkeipooh <k.chan.respect@gmail.com>
2021-07-07 09:22:51 +0900

    画像投稿機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,25 @@
 class ItemsController < ApplicationController
 
   def index
+    @items = Item.order("created_at DESC")
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :name, :text, :category_id, :price).merge(user_id: current_user.id)
   end
 
 
