a83a67cf1a44e5ce3196cda9238dfdef8ffb8e03
jinkeipooh <k.chan.respect@gmail.com>
2021-07-19 21:55:39 +0900

    Basic導入の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def current_cart
@@ -18,24 +19,13 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  # def current_cart
-  #   if user_signed_in?
-  #     if session[:cart_id]
-  #       current_cart = Cart.find_by(id: session[:cart_id])
-  #       session[:cart_id] = current_cart.id
-  #       current_cart
-  #     else
-  #       current_cart = Cart.create(user_id: current_user.id)        ###  current_user → ログインしていないユーザーが入れない！
-  #       session[:cart_id] = current_cart.id
-  #       current_cart = Cart.find_by(id: session[:cart_id])
-  #       current_cart
-  #     end
-  #   else
-  #     render :index                                               ###  renderで引き戻しているが、、これで良いのか？？？？？？？？？
-  #   end
-  # end
-
   private
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name, :first_name, :birthday])
   end
