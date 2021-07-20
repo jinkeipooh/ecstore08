225337b43fcebb230941db7143cac4ec24de18bd
jinkeipooh <k.chan.respect@gmail.com>
2021-07-20 16:48:21 +0900

    READMEの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -15,7 +15,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path
+      redirect_to user_path(@item.user.id)
     else
       render :new
     end
