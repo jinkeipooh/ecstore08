ecfb8936d3d22aef954f2823406fbb20a93077e0
jinkeipooh <k.chan.respect@gmail.com>
2021-07-22 18:34:25 +0900

    クレジット事前登録軽微な修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,9 @@ class UsersController < ApplicationController
 
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     card = Card.find_by(user_id: current_user.id)
-    redirect_to new_card_path and return unless card.present?
-    customer = Payjp::Customer.retrieve(card.customer_token) # 先程のカード情報を元に、顧客情報を取得
-    @card = customer.cards.first
+    if card.present?
+      customer = Payjp::Customer.retrieve(card.customer_token) # 先程のカード情報を元に、顧客情報を取得
+      @card = customer.cards.first
+    end
   end
 end
