c3e2ce7e4e75a6ffb680b3ef6685b605779edcd6
jinkeipooh <k.chan.respect@gmail.com>
2021-08-05 12:03:31 +0900

    アドレス情報の登録実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: 'users/registrations'
   }
+  devise_scope :user do
+    get 'addresses', to: 'users/registrations#new_address'
+    post 'addresses', to: 'users/registrations#create_address'
+  end
   root to: 'items#index'
   resources :users, only:[:show]
   resources :items
